a411abfbff2babc7574aac80bdf753dee554f060
Edgar Limon <edgarlimon805@gmail.com>
2022-08-04 09:53:19 -0700

    First Commit

---
+++ js/slideshow.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides();
+
+// Next $ Previous Controls
+function plusSlides(n) {
+  showSlides(slideIndex = n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
